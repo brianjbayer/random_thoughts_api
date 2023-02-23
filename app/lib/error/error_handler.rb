2e98df48c12995db7f6c4cb8215e24b04c9fe33f
brianjbayer <brianjbayer@gmail.com>
2023-02-23 10:09:16 -0500

    Use Rack::Utils to map error to code in render_error_response

--- app/lib/error/error_handler.rb
+++ app/lib/error/error_handler.rb
@@ -10,28 +10,27 @@ module Error
       including_class.class_eval do
         # Default catch-all exception
         rescue_from StandardError do |e|
-          render_response(500, :internal_server_error, e.to_s)
+          render_error_response(:internal_server_error, e.to_s)
         end
 
         # Expected exceptions
         rescue_from ActiveRecord::RecordNotFound do |e|
-          render_response(404, :not_found, e.to_s)
+          render_error_response(:not_found, e.to_s)
         end
 
         rescue_from ActiveRecord::RecordInvalid do |e|
-          render_response(422, :unprocessable_entity, e.to_s)
+          render_error_response(:unprocessable_entity, e.to_s)
         end
 
         rescue_from ActionController::ParameterMissing,
                     ActionDispatch::Http::Parameters::ParseError do |e|
-          render_response(400, :bad_request, e.to_s)
+          render_error_response(:bad_request, e.to_s)
         end
       end
     end
 
-    private
-
-    def render_response(status, error, message)
+    def render_error_response(error, message)
+      status = Rack::Utils.status_code(error)
       json = { status:, error:, message: }.to_json
       render json:, status:
     end
